d604ac9e16bab4b29ecde4afec7bbf2bd54ca4a4
Toan Nguyen <toan.nt181@gmail.com>
2026-06-07 10:03:46 +0700

    fix: npm build ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ tsconfigPath: './tsconfig.app.json', outDirs: ['dist'], entryRoot: 'src' }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
